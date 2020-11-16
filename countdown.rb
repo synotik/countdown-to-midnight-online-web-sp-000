511be723182f223b478404fdd082a0c946ad7fbb
Jeff Johnson <synotik@hotmail.com>
2020-11-16 04:19:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(number)
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+
+  return "HAPPY NEW YEAR!"
+
+end
